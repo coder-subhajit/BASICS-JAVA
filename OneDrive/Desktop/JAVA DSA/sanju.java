293e0c8fc94147dd0ba0293d8c362fc373101a7b
coder-subhajit <subhajitbarik909@gmail.com>
2026-02-18 12:30:36 +0530

    Added the full code

---
+++ OneDrive/Desktop/JAVA DSA/sanju.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class sanju {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter the radius of the sphere:");
+       double r=sc.nextDouble();
+
+        System.out.print("The area of a sphere is:\n");
+         double $_r1SAN= (4*Math.PI*r*r);
+        System.out.printf("%.2f",$_r1SAN);
+        
+
+        
+
+    }
+}
