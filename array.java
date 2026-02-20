6fd13998fad715237e960940009b4ca3aa8424e6
coder-subhajit <subhajitbarik909@gmail.com>
2026-02-20 12:38:15 +0530

    ARRAY INPUT

---
+++ array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class array{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the size of the array:");
+    int n=sc.nextInt();
+    int arr[]=new int[n] ;
+    System.out.println("Enter the "+n+" elements:");
+    for(int i=0;i<arr.length;i++){
+        arr[i]=sc.nextInt();
+    }
+    System.out.println("Array elements are:");
+    for(int i=0;i<arr.length;i++){
+        System.out.print( arr[i]+" ");
+    } sc.close();
+
+    
+    }
+}
