f7759618658a4ec5a495e012f47a3613bb7fe3e2
coder-subhajit <subhajitbarik909@gmail.com>
2026-07-16 12:25:56 +0530

    Restore folder structure removed by bad merge

---
+++ OneDrive/Desktop/JAVA DSA/prime.java
@@ -0,0 +1,29 @@
+public class prime {
+    public static void main(String args[]){
+    int n=5;
+    System.out.println(n);
+      n=99;
+    System.out.println(n);
+     String  n1 ="Subha";
+     System.out.println(n1);
+      n1 ="Rahul";
+     System.out.println(n1);
+     double n2=2.0;
+     System.out.println(n2);
+     float n3=6.0f;
+     System.out.println(n3);
+     String n4="nela";
+     System.out.println(n4);
+     long n5=999999;
+     System.out.println(n5);
+     boolean ismine=true;
+     if(ismine){
+        System.out.println("You are mine");}
+        short n6=12000;
+        System.out.println(n6);
+        byte age=120;
+        System.out.println(age);
+
+
+} 
+    }
