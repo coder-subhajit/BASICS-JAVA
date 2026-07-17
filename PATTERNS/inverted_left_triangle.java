8d637d0f4d715fd161b0bef4f3fc5a05ab94c40e
coder-subhajit <subhajitbarik909@gmail.com>
2026-07-17 20:18:32 +0530

    Inverted Left Triangle

---
+++ PATTERNS/inverted_left_triangle.java
@@ -0,0 +1,15 @@
+public class inverted_left_triangle {
+    public static void main(String[] args) {
+        int[] testCases={3,4};
+        for(int n  : testCases){
+            System.out.println("Pattern for n="+n);
+            for(int i=n;i>=1;i--){
+                for(int j=1;j<=n-i;j++){
+                    System.out.print(" ");
+                } for(int k=1;k<=i;k++){
+                    System.out.print("*");
+                } System.out.println();
+            }
+        }
+    }
+}
