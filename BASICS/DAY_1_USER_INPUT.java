627b2188f2cdb027a9a7f18cd106d17bba698846
CODER__SUBHAJIT <subhajitbarik909@gmail.com>
2026-07-23 11:59:26 +0530

    First Day Practice

---
+++ BASICS/DAY_1_USER_INPUT.java
@@ -0,0 +1,32 @@
+public class DAY_1_USER_INPUT {
+    public static void main(String args[]){
+    int n=5;
+    System.out.println(n);
+      n=99;
+      n=23456;
+    System.out.println(n);
+     String  n1 ="Subha";
+     System.out.println(n1);
+      n1 ="Rahul";
+     System.out.println(n1);
+     double n2=2.0;
+     System.out.println(n2);
+     float n3=6.0f;
+     System.out.println(n3);
+     String n4="Apple";
+     System.out.println(n4);
+     long n5=999999;
+     System.out.println(n5);
+     boolean ismine=true;
+     if(ismine){
+        System.out.println("Her&I");}
+        short n6=12000;
+        System.out.println(n6);
+        byte age=120;
+        System.out.println(age);
+
+
+} 
+    }
+
+
