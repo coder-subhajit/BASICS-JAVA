c5d9be2371603a0413dae764cff0c1036fe5b99e
CODER__SUBHAJIT <subhajitbarik909@gmail.com>
2026-07-23 11:51:41 +0530

    Enhance User_Input.java with comments and formatting
    Added comments for clarity and improved code readability.

--- ARRAY LIST/User_Input.java
+++ ARRAY LIST/User_Input.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class User_Input {
     public static void main(String[] args) {
       ArrayList<Integer>list=new ArrayList<>();
+      System.out.println(list);//print nothing this is empty list
+
       //ADD ELEMENTS  
       list.add(0);
       list.add(1);
@@ -13,34 +15,52 @@ public class User_Input {
       list.add(1,4);
       System.out.println(list); 
 
-    //GET ELEMENTS
+    //GET ELEMENTS BY INDEX
     int element=list.get(0);
     System.out.println(element);
       element=list.get(3);
       System.out.println(element);//output comes from the updated list
+
+      //CHECKING ELEMENTS IS PRESENT IN THE LIST
+      System.out.println(list.contains(5));
    
-    //MODIFY OR CHANGE THE VALUE OF LIST
+    //MODIFY OR CHANGE THE VALUE OF LIST OR UPDATE AN ELEMENTS
     list.set(0,9);
     System.out.println(list);
 
-    //DELETE OR REMOVE ELEMENT FROM LIST
+    //DELETE OR REMOVE ELEMENT BY INDEX FROM LIST
     list.remove(0);
     list.remove(2);
     System.out.println(list);
+
+    //REMOVE BY VALUE
+    list.remove(Integer.valueOf(4));
 
     //FIND THE SIZE OF THE LIST
     int size=list.size();
     System.out.println("The size of the list is:"+size);
      
     //USE LOOPS
-    System.out.println("The element of the list is:");
+    System.out.println("The element of the list is(use normal loop):");
     for(int i=0;i<list.size();i++){
       System.out.println(list.get(i)+" ");
+    }
+    //USE FOR EACH LOOPS
+     System.out.println("The element of the list is(using for each loop):");
+    for(int num : list){
+      System.out.println(num);
     }
 
     //PERFORM SORTING USING COLLCTIONS PACKAGE
     System.out.println("The sorted order of list is:");
     Collections.sort(list);
     System.out.println(list);
+
+    //CLEAR THE ENTIRE LIST
+    list.clear();
+    System.out.println(list);
+
+    //CHECKS IF THE LIST CONTAINS ZERO ELEMENT
+    System.out.println(list.isEmpty());
 }
 }
