9c59d58820fd0d5e01b430df9c6c359ba20d6f91
CODER__SUBHAJIT <subhajitbarik909@gmail.com>
2026-08-23 03:11:27 +0530

    Create final Vehicle class to restrict inheritance
    Added final class Vehicle to prevent inheritance.

---
+++ OOPS/final_KEYWORD/Vehicle.java
@@ -0,0 +1,9 @@
+//final class
+public final class Vehicle {
+    void display(){
+        System.out.println("Vehicle class...");
+    }
+}
+public class Car extends Vehicle{
+    //cannot inherit
+}
