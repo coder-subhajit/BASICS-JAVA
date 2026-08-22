cf8f30241605544c23435b30d6b860fe875cbed0
coder-subhajit <subhajitbarik909@gmail.com>
2026-08-23 03:09:20 +0530

    Add Codes

---
+++ OOPS/final_KEYWORD/Animal.java
@@ -0,0 +1,6 @@
+//final method
+public class Method {
+   final void sound(){
+    System.out.println("Animal")
+   } 
+}
---
+++ OOPS/final_KEYWORD/Variable.java
@@ -0,0 +1,8 @@
+//final variable
+public class Variable {
+    public static void main(String[] args) {
+        final int x=10;
+        // x=20; cannot assign value
+        System.out.print(x);
+    }
+}
