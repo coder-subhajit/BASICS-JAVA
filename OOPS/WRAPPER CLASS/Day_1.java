524a17eaf24fb4175d904a0df7b5e65175818b5a
coder-subhajit <subhajitbarik909@gmail.com>
2026-08-23 02:11:11 +0530

    Add Codes

---
+++ OOPS/WRAPPER CLASS/Day_1.java
@@ -0,0 +1,15 @@
+//Example of Wrapper Classes
+public class Day_1 {
+  public static void main(String[] args){
+    //primitive data type
+    int num=10;
+    //Autoboxing
+    Integer obj=num;
+    //Unboxing
+    int value=obj;
+
+    System.out.println("Primitive:"+num);
+    System.out.println("Wrapper Object:"+obj);
+    System.out.println("Unboxed Value:"+value);
+  }  
+}
---
+++ OOPS/WRAPPER CLASS/Utilty_Methods.java
@@ -0,0 +1,13 @@
+public class Utilty_Methods{
+    public static void main(String[] args) {
+        String str="123";
+        //String to int
+        int n=Integer.parseInt(str);
+        //int to string
+        String s=Integer.toString(n);
+        
+        System.out.println(str);
+        System.out.println(n);
+        System.out.println(s);
+    }
+}
